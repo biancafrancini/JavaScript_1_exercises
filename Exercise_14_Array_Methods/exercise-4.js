64315d0bdcc3b04b8e12188748394383de476837
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-11-06 10:02:31 +0000

    added Exercises about Array Methods

---
+++ Exercise_14_Array_Methods/exercise-4.js
@@ -0,0 +1,63 @@
+function uncompletedNotes(notes) {
+  let newTodosArray = [];
+  notes.forEach((obj) =>
+    obj.todos.filter((element) => {
+      if (element.done === false) {
+        return newTodosArray.push(element);
+      }
+    })
+  );
+  console.log(newTodosArray);
+}
+
+const notes = [
+  {
+    id: 1,
+    description: "Workout program",
+    todos: [
+      {
+        id: 1,
+        name: "Push ups - 10 x 3",
+        done: false,
+      },
+      {
+        id: 2,
+        name: "Abdominals - 20 x 3",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Tapis Roulant - 15min",
+        done: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    description: "Front-end Roadmap",
+    todos: [
+      {
+        id: 1,
+        name: "Learn HTML",
+        done: true,
+      },
+      {
+        id: 2,
+        name: "Learn CSS",
+        done: true,
+      },
+      {
+        id: 3,
+        name: "Learn JavaScript",
+        done: true,
+      },
+      {
+        id: 4,
+        name: "Learn Angular",
+        done: false,
+      },
+    ],
+  },
+];
+
+uncompletedNotes(notes);
