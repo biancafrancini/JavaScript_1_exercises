d50b1dea50762c2b2029419ce324717f79eabca9
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-11-14 16:06:48 +0000

    added map method

--- Exercise_11_Array_methods/exercise-1.js
+++ Exercise_11_Array_methods/exercise-1.js
@@ -1,24 +1,24 @@
 function adultFilter(persons) {
   let newArrayPersons = [];
-  for(let i = 0; i < persons.length; i++){
-    if(persons[i].age >= 18){
-      newArrayPersons.push(persons[i]);
+  persons.map((element) => {
+    if (element.age >= 18) {
+      return newArrayPersons.push(element);
     }
-  }
+  });
   return newArrayPersons;
 }
 
 const persons = [
-  { name: 'Paul', age: 16 },
-  { name: 'George', age: 17 },
-  { name: 'Lucas', age: 21 },
-  { name: 'Marco', age: 32 },
-  { name: 'Peter', age: 18 },
-  { name: 'Carl', age: 13 },
-  { name: 'Simon', age: 24 },
-  { name: 'Mark', age: 15 },
-  { name: 'Sandra', age: 34 },
-  { name: 'Alice', age: 28 }
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
 ];
 
 const adults = adultFilter(persons);
